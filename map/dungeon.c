016faa5272151361edbd79e5d0fa897178168232
minseob22 <minseobhan22@gmail.com>
2024-11-06 11:15:45 +0900

    init

--- map/dungeon.c
+++ map/dungeon.c
@@ -5,7 +5,7 @@ void initialize_dungeon(Dungeon *dungeon) {
     dungeon->x = 0;
     dungeon->y = 0;
 
-    // 이벤트 초기화 예시
+    // Initialize events (example)
     dungeon->map[1][1] = EVENT_ENEMY;
     dungeon->map[2][2] = EVENT_CHEST;
 }
@@ -16,24 +16,24 @@ void move_party(Dungeon *dungeon, char direction) {
         case 'S': if (dungeon->y < DUNGEON_SIZE - 1) dungeon->y++; break;
         case 'E': if (dungeon->x < DUNGEON_SIZE - 1) dungeon->x++; break;
         case 'W': if (dungeon->x > 0) dungeon->x--; break;
-        default: printf("잘못된 방향입니다.\n"); return;
+        default: printf("Invalid direction.\n"); return;
     }
-    printf("파티가 (%d, %d) 위치로 이동했습니다.\n", dungeon->x, dungeon->y);
+    printf("The party moved to position (%d, %d).\n", dungeon->x, dungeon->y);
 }
 
 void handle_event(Dungeon *dungeon) {
     int event = dungeon->map[dungeon->y][dungeon->x];
     switch (event) {
         case EVENT_NONE:
-            printf("이곳에는 아무것도 없습니다.\n");
+            printf("There is nothing here.\n");
             break;
         case EVENT_ENEMY:
-            printf("적을 만났습니다! 전투를 시작합니다.\n");
-            // 전투 시작 코드 (start_battle 함수 호출)
+            printf("You encountered an enemy! Starting battle.\n");
+            // Code to start battle (call start_battle function)
             break;
         case EVENT_CHEST:
-            printf("보물 상자를 발견했습니다!\n");
-            // 보물 획득 코드
+            printf("You found a treasure chest!\n");
+            // Code to obtain treasure
             break;
     }
 }
--- map/village.c
+++ map/village.c
@@ -3,13 +3,13 @@
 
 void initialize_village(Village *village) {
     village->npc_count = 3;
-    snprintf(village->npcs[0].name, sizeof(village->npcs[0].name), "장비 상인");
-    snprintf(village->npcs[1].name, sizeof(village->npcs[1].name), "물약 상인");
-    snprintf(village->npcs[2].name, sizeof(village->npcs[2].name), "퀘스트 NPC");
+    snprintf(village->npcs[0].name, sizeof(village->npcs[0].name), "Equipment Merchant");
+    snprintf(village->npcs[1].name, sizeof(village->npcs[1].name), "Potion Merchant");
+    snprintf(village->npcs[2].name, sizeof(village->npcs[2].name), "Quest NPC");
 }
 
 void village_menu(Village *village) {
-    printf("마을에 도착했습니다. 무엇을 하시겠습니까?\n");
+    printf("You have arrived in the village. What would you like to do?\n");
     for (int i = 0; i < village->npc_count; i++) {
         printf("%d. %s\n", i + 1, village->npcs[i].name);
     }
@@ -18,6 +18,6 @@ void village_menu(Village *village) {
     if (choice >= 1 && choice <= village->npc_count) {
         interact_with_npc(&village->npcs[choice - 1]);
     } else {
-        printf("잘못된 선택입니다.\n");
+        printf("Invalid choice.\n");
     }
 }
